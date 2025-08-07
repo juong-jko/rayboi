f54251431b8345e5727a6189b77f88fc1d90664f
juong-jko <juong.jko@gmail.com>
2025-08-06 22:37:14 -0700

    Add registers

---
+++ notes.md
@@ -0,0 +1,15 @@
+## Basic Game Boy Computer Architecture
+### CPU
+- Executing instructions
+- Memory I/O
+- Invoking event (interrupt) handlers from I/O
+- Keep track of registers to perform computations and manipulations
+
+### ROM (Read-Only Memort)
+- Hardcoded bits
+- Tells the machine how to set itself up
+
+### IO Components
+- The Screen
+- Speaker
+- Gamepad
---
+++ src/registers.rs
@@ -0,0 +1,29 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    g: u8,
+    h: u8,
+}
+
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+
+impl Registers {
+    fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8
+        | self.c as u16
+    }
+
+    fn set_bc(&mut self, value: u16) {
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+}
